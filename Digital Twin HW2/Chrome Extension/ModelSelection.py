cd2bc070b08952d47b5087b4a5449af534da5ab4
cpennington-de <65792142+cpennington-de@users.noreply.github.com>
2024-12-04 22:00:28 -0600

    Added comments showing where more code will go Specifically a model and a data extraction feature Plus modified the popup so that it would look nice

---
+++ Digital Twin HW2/Chrome Extension/ModelSelection.py
@@ -0,0 +1,2 @@
+#placeholder file for a potential python program that could host various models
+#Would ultimatly return the best model to use base on data given to it.
--- Digital Twin HW2/Chrome Extension/server.py
+++ Digital Twin HW2/Chrome Extension/server.py
@@ -5,6 +5,18 @@ import re
 app = Flask(__name__)
 CORS(app)  # Enable CORS for the entire app
 
+def Feature_Extraction(text):
+    #if we decide to do the feature extraction using python use this as the spot
+    #put code that extracts features from the website
+
+    return ""
+def Phishing_Detection_Model(Extracted_DATA):
+    #put in the code for the chose phsihing detection model
+    #the output of the function should return a True or fast value 
+    #based on whether the data shows the website is a phishing website or not
+        return True
+
+
 # This function runs each time a piece of text is posted to the server
 # It now returns True if "Google" is in the text and False otherwise
 @app.route('/process_text', methods=['POST'])
